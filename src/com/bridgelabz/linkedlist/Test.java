a2ce54a0e8541b8509573bb49893c2e0f3a8f566
DivyaMundhada <divyamundada9@gmail.com>
2021-07-10 10:56:13 +0530

    Added Feature to Create a Simple Linked List.

--- src/com/bridgelabz/linkedlist/Test.java
+++ src/com/bridgelabz/linkedlist/Test.java
@@ -3,5 +3,14 @@ package com.bridgelabz.linkedlist;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Welcome to LinkedList Program.");
+		Node myFirstNode = new Node(56);
+		Node mySecondNode = new Node(30);
+		Node myThirdNode = new Node(50);
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+
+		boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+		System.out.print(result);
+
 	}
 }
